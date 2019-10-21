93bcbeb1ce5c6cd6bc042d0e0304338ef6edf9f7
josieyao <yao.josephine@yahoo.com>
2019-10-21 10:05:57 -0500

    created metrics reducers/actions

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
-import NowWhat from "./components/NowWhat";
+// import NowWhat from "./components/NowWhat";
+import SelectDropDown from "./components/SelectDropDown";
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: "https://react.eogresources.com/graphql",
+});
 
 const store = createStore();
+
 const theme = createMuiTheme({
   typography: {
     useNextVariants: true
@@ -28,16 +36,19 @@ const theme = createMuiTheme({
 });
 
 const App = props => (
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    <Provider store={store}>
-      <Wrapper>
-        <Header />
-        <NowWhat />
-        <ToastContainer />
-      </Wrapper>
-    </Provider>
-  </MuiThemeProvider>
+  <ApolloProvider client={client}>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Provider store={store}>
+        <Wrapper>
+          <Header />
+          <SelectDropDown/>
+          {/* <NowWhat /> */}
+          <ToastContainer />
+        </Wrapper>
+      </Provider>
+    </MuiThemeProvider>
+  </ApolloProvider>
 );
 
 export default App;
