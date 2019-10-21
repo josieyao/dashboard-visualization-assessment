76d00d7d9deff815f6ea553f3b45a93f56363ea4
josieyao <yao.josephine@yahoo.com>
2019-10-21 10:19:27 -0500

    multi select box working with graphql queries

---
+++ src/components/SelectDropDown.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as actions from "../store/actions";
+import { Provider, createClient, useQuery } from "urql";
+import Multiselect from 'multiselect-dropdown-react';
+import LinearProgress from "@material-ui/core/LinearProgress";
+
+
+    const client = createClient({
+        url: "https://react.eogresources.com/graphql"
+      });
+    
+    const query = `{
+        getMetrics
+    }`
+    
+    export default()=>{
+        return(
+            <Provider value = {client}>
+              <SelectDropDown/>
+            </Provider>
+        )
+      }
+       
+    const SelectDropDown = () =>{
+        const dispatch = useDispatch();
+       
+       //fetch all the metrics
+       const [result] = useQuery({
+           query
+       })
+       const {fetching, data, error} = result;
+       const metricsData = []
+    
+       useEffect(()=>{
+           if(error){
+               dispatch({type:actions.API_ERROR, error: error.message});
+               return;
+           }
+           if(!data) return;
+           const {getMetrics} = data
+        //    console.log(getMetrics)
+           getMetrics.map(metric => metricsData.push({name:`${metric}`, value:`${metric}`}))
+        //    console.log(metricsData)
+           dispatch({type: actions.SELECT_DROP_DOWN_DATA_RECEIVED, getMetrics});
+          },
+        //[ data ]
+      );
+
+        //filter metrics
+        const selectMetrics = (options) => {
+        dispatch({ type: actions.SELECTED_METRICS_RECEIVED, options })
+        }
+
+
+
+      if(fetching) return <LinearProgress/>
+    
+      return(
+            <div>
+                <Multiselect options={metricsData} onSelectOptions={selectMetrics} />
+            </div>
+        ) 
+    }
